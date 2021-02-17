1e23260255f7f29ecb370946c411bd6a8a08f78a
zegildo <zegildo@gmail.com>
2021-02-17 10:21:38 -0300

    melhoria nos testes

--- FlightSurety-master/test/flightSurety.js
+++ FlightSurety-master/test/flightSurety.js
@@ -78,9 +78,11 @@ contract('Flight Surety Tests', async (accounts) => {
     assert.equal(num_airlines, 1);
    
     let airline_details = await instanceApp.getAirline(config.owner);
+    //console.log(airline_details);
     
     assert.equal(airline_details[1], "Latam Airlines");
-    assert.equal(airline_details[2], true);
+    assert.equal(airline_details[2], true);//isRegistered
+    assert.equal(airline_details[3], false);//isFunded
   });
 
   it("(airline register) register 4 airlines directly", async() => {
@@ -92,6 +94,13 @@ contract('Flight Surety Tests', async (accounts) => {
     
     // first airline registers two consecutive airlines
     await instanceApp.registerAirline(airline_2, "Azul Airlines", {from:config.owner});
+    
+    try{
+        await instanceApp.registerAirline(airline_2, "Azul Airlines", {from:config.owner});
+    }catch(err){
+        assert.equal(err.reason, "Airline is already registered");
+    }
+
     await instanceApp.registerAirline(airline_3, "American Airlines", {from:config.owner});
     let num_airlines = await instanceApp.getNumAirlinesRegistred.call();    
     assert.equal(num_airlines, 3);
@@ -109,14 +118,24 @@ contract('Flight Surety Tests', async (accounts) => {
 
     assert.equal(airline_details_1[1], "Latam Airlines");
     assert.equal(airline_details_1[2], true);
+    assert.equal(airline_details_1[3], false);
+    
     assert.equal(airline_details_2[1], "Azul Airlines");
     assert.equal(airline_details_2[2], true);
+    assert.equal(airline_details_2[3], false);
+    
+    
     assert.equal(airline_details_3[1], "American Airlines");
     assert.equal(airline_details_3[2], true);
+    assert.equal(airline_details_3[3], false);
+    
+    
     assert.equal(airline_details_4[1], "Arabian Airlines");
     assert.equal(airline_details_4[2], true);
+    assert.equal(airline_details_4[3], false);
     
 });
+
 
 it("(airline register) 5 airlines is added but Not-registered", async() => {
     let instanceApp = await config.flightSuretyApp;
@@ -130,17 +149,13 @@ it("(airline register) 5 airlines is added but Not-registered", async() => {
 
     assert.equal(airline_details[1], "Jet Leg Arilines");
     assert.equal(airline_details[2], false);
+    assert.equal(airline_details[3], false);
 });
 
-
-
-it("(multiparty/airline register) register by multiparty consensus voting", async() => {
+it("(voting) register by multiparty consensus voting", async() => {
     
     let instanceApp = await config.flightSuretyApp;
     let num_airlines = await instanceApp.getNumAirlinesRegistred.call();
-    
-    // there are 5 airlines in the list
-    assert.equal(num_airlines, 5);
     
     let airline_2 = accounts[1];
     let airline_3 = accounts[2];
@@ -150,8 +165,6 @@ it("(multiparty/airline register) register by multiparty consensus voting", asyn
     //5th airline is not registered
     let airline_5_details = await instanceApp.getAirline.call(airline_5);
     assert.equal(airline_5_details[2], false);
-
-    
 
     //2sd airline is already registered
     let airline_2_details = await instanceApp.getAirline.call(airline_2);
@@ -169,21 +182,40 @@ it("(multiparty/airline register) register by multiparty consensus voting", asyn
     assert.equal(owner_details[2], true);
 
     try{
-        await instanceApp.vote(airline_5, {from: config.owner});
+        await instanceApp.vote(airline_5, {from:config.owner});
     }catch(err){
         assert.equal(err.reason,"Airline does not participate in contract until it submits funding of 10 ether");
     }
+    //now can vote because has paid the fund
+    let ten_ether_fund = await web3.utils.toWei("10", "ether");
+    await instanceApp.fund({from:config.owner, value:ten_ether_fund});
+    
+    await instanceApp.vote(airline_5, {from:config.owner});
+    airline_5_details = await instanceApp.getAirline.call(airline_5);
+    assert.equal(airline_5_details[2], false);
+    assert.equal(airline_5_details[4].length, 1);
 
-
-
-
-    //await instanceApp.fund({from:airline_2, value:5});
-    //TODO CONTINUIN CHECK VOTING
-    //IF THE SAME COMPANY CAN VOTE AGAIN
+    try{
+        await instanceApp.vote(airline_5, {from:config.owner});
+    }catch(e){
+        assert.equal(err.reason,"The Airline already voted!");
+    }
     //IF MORE THE 50% REGISTER THE COMPANY
+    await instanceApp.vote(airline_5, {from:airline_2});
+    await instanceApp.vote(airline_5, {from:airline_3});
+
+    airline_5_details = await instanceApp.getAirline.call(airline_5);
+    // there are 5 airlines in the list
+    assert.equal(num_airlines, 5);
+    //3 of 5 (more than 50%)
+    assert.equal(airline_5_details[4].length, 3);
+    //isRegistered is true
+    assert.equal(airline_5_details[2], true);
+     
+    
 
 });
-
+/*
 it("(Flight) create a flight", async() => {
     
     //https://www.kwe.co.jp/en/useful-contents/code1
@@ -218,6 +250,7 @@ it("(Flight) Update Status", async() => {
     let flight_code = "AAL001";
     let date = "2020-06-09T12:00:00Z"
     let timestamp = new Date(date).getTime();
+    console.log("timestamp:",timestamp);
 
     let flight_hash = await instanceApp.getFlightKey(config.owner, flight_code, timestamp);
 
@@ -284,18 +317,23 @@ it("(Buy an insure) ", async() => {
 });
 
 it("(Credit Insuree) ", async() => {
-    let instanceApp = await FlightSuretyApp.deployed();
-   
-    let user_client = accounts[6];
-    
-    let airline = config.owner;
+    let instanceApp = await config.flightSuretyApp;
+
     let flight_code = "AAL001";
+    let origin = "Rio";
+    let destination = "London";
     let date = "2020-06-09T12:00:00Z"
     let timestamp = new Date(date).getTime();
+
+    await instanceApp.registerFlight(flight_code,origin,destination,timestamp,{from:config.owner});
+
+    let flight_hash = await instanceApp.getFlightKey.call(config.owner, flight_code, timestamp);
+   
+    let user_client = accounts[6];
+    let airline = config.owner;
     let statusCode = 20;
     
     await instanceApp.processFlightStatus(airline, flight_code, timestamp, statusCode, {from:user_client});
-    let flight_hash = await instanceApp.getFlightKey(airline_address, fligh_code, timestamp);
     let insure_value = await instanceApp.getInsuredClient(flight_hash, {from:user_client});
     let insure_due = await instanceApp.getInsuredDue(flight_hash, {from:user_client});
     assert.equal(insure_due, insure_value * 1.5);
@@ -304,10 +342,16 @@ it("(Credit Insuree) ", async() => {
 
 it("(Pay Insuree) ", async() => {
 
-    let instanceApp = await FlightSuretyApp.deployed();
+    let instanceApp = await config.flightSuretyApp;
 
     let user_client = accounts[6];
     let value = await web3.utils.toWei("0.2", "ether");
+    let airline = config.owner;
+    let flight_code = "AAL001";
+    let date = "2020-06-09T12:00:00Z"
+    let timestamp = new Date(date).getTime();
+
+    let flight_hash = await instanceApp.getFlightKey.call(airline, flight_code, timestamp);
 
     let user_balance_before = await web3.eth.getBalance(user_client);
     let insure_due_before = await instanceApp.getInsuredDue(flight_hash, {from:user_client});
@@ -322,6 +366,6 @@ it("(Pay Insuree) ", async() => {
 
 });
 
- 
+*/ 
 
 });
