3f4f5884e151c8c64d0d7381c3decf4edf9477dd
zegildo <zegildo@gmail.com>
2021-03-24 10:55:40 -0300

    truffle-config.js

--- Blockchain-Capstone-master/eth-contracts/truffle-config.js
+++ Blockchain-Capstone-master/eth-contracts/truffle-config.js
@@ -19,10 +19,8 @@
  */
 
 const HDWalletProvider = require('@truffle/hdwallet-provider');
-//const infuraKey = "fj4jll3k.....";
-
-const fs = require('fs');
-//const mnemonic = fs.readFileSync(".secret").toString().trim();
+const infuraKey = "287c457c6b29464b8a99f9de739a01e2";
+const mnemonic = "gold romance magic wheat become grocery lunch craft auction jazz table fiction";
 
 module.exports = {
   /**
@@ -58,13 +56,11 @@ module.exports = {
     // },
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
-     ropsten: {
-      provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-      network_id: 3,       // Ropsten's id
-      gas: 5500000,        // Ropsten has a lower block limit than mainnet
-      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    rinkeby: {                                                                                    //use the second account
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`, 1),
+      network_id: 4       // rinkeby's id
+      //gas: 5500000,        // rinkeby has a lower block limit than mainnet
+      //gasPrice: 10000000000
      },
     // Useful for private networks
      private: {
